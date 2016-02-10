2266241995c636587474af995a432baf7fcc1231
awoodcroft95 <a.g.woodcroft@hotmail.com>
2016-02-10 20:22:44 +0000

    Java Doc and Comments

---
+++ src/ProjectManagementSoftware/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProjectManagementSoftware;
+
+/**
+ *
+ * @author Anthony
+ */
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        Date projectStart = new Date();
+        
+    }
+    
+}
---
+++ src/ProjectManagementSoftware/PertItem.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProjectManagementSoftware;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Anthony
+ */
+public class PertItem {
+    
+    public Date startDate = new Date();
+    public Date endDate = new Date();
+    public Date lateStart = new Date();
+    public Date lateEnd = new Date();
+    public boolean critAct; 
+    public int lengthDays;
+    public int stackDays;
+    public String ID;
+    public ArrayList predecessorID = new ArrayList();
+    public ArrayList successorID = new ArrayList();
+    
+    public PertItem() {
+    }
+    
+    public PertItem(Date startDate, Date endDate, boolean critAct){
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.critAct = critAct;
+    }
+    
+    public String getID() {
+        return this.ID;
+    }
+    
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+}
